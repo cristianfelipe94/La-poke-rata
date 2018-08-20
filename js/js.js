c1597f1b0d8c9dd068fc0e03e5678701152f838d
Luis Armando <luisarmando.murillobaltodano@gmail.com>
2018-08-19 18:51:34 -0600

    quitando el spanglish

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
-// Funciones
-let funciones = {
+// functions
+let functions = {
    
 
   start_game(){
@@ -28,80 +28,80 @@ let funciones = {
   },
 
 
-  resta_al_enemigo_1(){
-    funciones.manejo_de_turnos();
-    vida_enemigo -= 0;
-    funciones.controlar_vida();
-    barra_interna[0].style.setProperty('width',vida_enemigo * 10 + 'px');
-    if(vida_enemigo > 0){
+  less_enemy_life_1(){
+    functions.turn_management();
+    enemy_life -= 0;
+    functions.control_life();
+    inside_bar[0].style.setProperty('width',enemy_life * 10 + 'px');
+    if(enemy_life > 0){
       text.innerHTML = name[1].innerHTML + ' le ha lanzado una '  + action[0].innerHTML.toLowerCase() + ' a ' + name[0].innerHTML + '.<br/>' + name[0].innerHTML + ' se la comió.';
     }
-    change_numbers_1.innerHTML = vida_enemigo;
+    change_numbers_1.innerHTML = enemy_life;
   },
 
 
-  resta_al_enemigo_2(){
-    funciones.manejo_de_turnos();
-    vida_enemigo -= 8;
-    funciones.controlar_vida();
-    barra_interna[0].style.setProperty('width',vida_enemigo * 10 + 'px');
-    if(vida_enemigo > 0){
+  less_enemy_life_2(){
+    functions.turn_management();
+    enemy_life -= 8;
+    functions.control_life();
+    inside_bar[0].style.setProperty('width',enemy_life * 10 + 'px');
+    if(enemy_life > 0){
       text.innerHTML = name[1].innerHTML + ' le ha lanzado  un ' + action[1].innerHTML.toLowerCase() + ' a ' + name[0].innerHTML + '.';
     }
-    change_numbers_1.innerHTML = vida_enemigo;
+    change_numbers_1.innerHTML = enemy_life;
   },
 
 
-  resta_al_enemigo_3(){
-    funciones.manejo_de_turnos();
-    vida_enemigo -= 1;
-    funciones.controlar_vida();
-    barra_interna[0].style.setProperty('width',vida_enemigo * 10 + 'px');
-    if(vida_enemigo > 0){
+  less_enemy_life_3(){
+    functions.turn_management();
+    enemy_life -= 1;
+    functions.control_life();
+    inside_bar[0].style.setProperty('width',enemy_life * 10 + 'px');
+    if(enemy_life > 0){
       text.innerHTML = name[1].innerHTML +  ' le ha lanzado un' + ataque_random[Math.floor(Math.random() * (ataque_random.length)) ] + ' a ' + name[0].innerHTML + '.';
     }
-    change_numbers_1.innerHTML = vida_enemigo;
+    change_numbers_1.innerHTML = enemy_life;
   },
 
 
-  resta_al_enemigo_4(){
-    funciones.manejo_de_turnos();
-    vida_enemigo -= 16;
-    funciones.controlar_vida();
-    barra_interna[0].style.setProperty('width',vida_enemigo * 10 + 'px');
-    if(vida_enemigo > 0){
+  less_enemy_life_4(){
+    functions.turn_management();
+    enemy_life -= 16;
+    functions.control_life();
+    inside_bar[0].style.setProperty('width',enemy_life * 10 + 'px');
+    if(enemy_life > 0){
       text.innerHTML = name[1].innerHTML + ' le ha lanzado un tremendo ' + action[3].innerHTML + ' a ' + name[0].innerHTML + '.<br/>Y ha sido super efectivo.';
     }
-    change_numbers_1.innerHTML = vida_enemigo;
+    change_numbers_1.innerHTML = enemy_life;
   },
 
 
 
-  controlar_vida(){
+  control_life(){
     //Para que la vida cambie de color
-    if(vida_enemigo <= 32){
-      barra_interna[0].setAttribute('class','barra_interna color_1');
+    if(enemy_life <= 32){
+      inside_bar[0].setAttribute('class','inside_bar color_1');
     }
-    if(vida_enemigo <= 20){
-      barra_interna[0].setAttribute('class','barra_interna color_2');
+    if(enemy_life <= 20){
+      inside_bar[0].setAttribute('class','inside_bar color_2');
     }
-    if(vida_enemigo <= 10){
-      barra_interna[0].setAttribute('class','barra_interna color_3');
+    if(enemy_life <= 10){
+      inside_bar[0].setAttribute('class','inside_bar color_3');
     }
     
-    if(vida_propia <= 32){
-      barra_interna[1].setAttribute('class','barra_interna color_1');
+    if(own_life <= 32){
+      inside_bar[1].setAttribute('class','inside_bar color_1');
     }
-    if(vida_propia <= 20){
-      barra_interna[1].setAttribute('class','barra_interna color_2');
+    if(own_life <= 20){
+      inside_bar[1].setAttribute('class','inside_bar color_2');
     }
-    if(vida_propia <= 10){
-      barra_interna[1].setAttribute('class','barra_interna color_3');
+    if(own_life <= 10){
+      inside_bar[1].setAttribute('class','inside_bar color_3');
     }
     //Para que la vida no sea valores negativos
-    if(vida_enemigo <= 0){
-      vida_enemigo = 0;
-      barra_interna[0].style.setProperty('border','0px solid');
+    if(enemy_life <= 0){
+      enemy_life = 0;
+      inside_bar[0].style.setProperty('border','0px solid');
       text.innerHTML = name[0].innerHTML + ' murió en la fría soledad de la esclavitud por su relativa fragilidad.<br/>';
       text.innerHTML += 'En el abismo de la violencia pokemón y sus peleas clandestinas.';
       window.setTimeout(function(){
@@ -113,35 +113,35 @@ let funciones = {
         final_space.style.setProperty('z-index','1000000');
       }, 3000);
     }
-    if(vida_propia <= 0){
-      barra_interna[1].style.setProperty('border','0px solid');
-      vida_propia = 0.1;
+    if(own_life <= 0){
+      inside_bar[1].style.setProperty('border','0px solid');
+      own_life = 0.1;
       text.innerHTML = name[1].innerHTML + ' murió porque fuiste un mal entrenador y una completa desgracia como ser humano.<br/>';
       window.setTimeout(function(){
         let a = 9;
         final_space.style.setProperty('opacity','1');
         final_space.style.setProperty('z-index','1000000');
   
-        function continuar(){
+        function keep_going(){
           final_space.style.setProperty('opacity','0');
           final_space.style.setProperty('z-index','-1000000');
           //Reinicia pelea
-          barra_interna[0].setAttribute('class','barra_interna color_1');
-          barra_interna[1].setAttribute('class','barra_interna color_1');
+          inside_bar[0].setAttribute('class','inside_bar color_1');
+          inside_bar[1].setAttribute('class','inside_bar color_1');
   
           change_numbers_1.innerHTML = 32;
           change_numbers_2.innerHTML = 32;
   
-          vida_enemigo = 32;
-          vida_propia = 32;
+          enemy_life = 32;
+          own_life = 32;
   
-          barra_interna[0].style.setProperty('width',vida_enemigo * 10 + 'px');
-          barra_interna[1].style.setProperty('width',vida_propia * 10 + 'px');
+          inside_bar[0].style.setProperty('width',enemy_life * 10 + 'px');
+          inside_bar[1].style.setProperty('width',own_life * 10 + 'px');
   
           grid_options[0].style.setProperty('display','grid');
           text.style.setProperty('width','35%');
   
-          funciones.cambiar_song();
+          functions.change_song();
   
           /*
             Uncaught (in promise) DOMException: The play() request was interrupted by a new load request.
@@ -150,7 +150,7 @@ let funciones = {
           text.innerHTML = "Vamos, que no te humillen de nuevo, el arroz es la clave.";
         }
         
-        function no_continuar(){
+        function no_keep_going(){
           final_space.style.setProperty('opacity','0');
           final_space.style.setProperty('z-index','-1000000');
           //Developers
@@ -158,43 +158,43 @@ let funciones = {
           the_wrapper.style.setProperty('top','-100vh');
           document.querySelector('.play_again').addEventListener('click',function(){
             the_wrapper.style.setProperty('top','0vh');
-            continuar();
+            keep_going();
             text.innerHTML = "¡¡A luchar esclavos!!";
           });
         }
         
-        button_ending[0].addEventListener('click',continuar);
+        button_ending[0].addEventListener('click',keep_going);
   
-        button_ending[1].addEventListener('click',no_continuar);
+        button_ending[1].addEventListener('click',no_keep_going);
 
       }, 3000);
     }
   },
 
 
-  manejo_de_turnos(){
-    let estado_del_turno = 0;
+  turn_management(){
+    let turn_moment = 0;
     grid_options[0].style.setProperty('display','none');
     text.style.setProperty('width','94%');
-    mostrar_options();
-    function mostrar_options(){
-      if(vida_enemigo != 0 || vida_propia != 0){
-        switch(estado_del_turno){
+    show_options();
+    function show_options(){
+      if(enemy_life != 0 || own_life != 0){
+        switch(turn_moment){
           case 0:
             grid_options[0].style.setProperty('display','none');  
             text.innerHTML = name[0].innerHTML + ' está enojado';
-            window.setTimeout(mostrar_options, 3000);
+            window.setTimeout(show_options, 3000);
             break;
           case 1:
             text.innerHTML = name[0].innerHTML + ' le ha lanzado un' + ataque_random[Math.floor(Math.random() * (ataque_random.length)) ] + ' a ' + name[1].innerHTML;
             grid_options[0].style.setProperty('display','none');
-            if(vida_enemigo != 0){
-              vida_propia -= 16;
+            if(enemy_life != 0){
+              own_life -= 16;
             }
-            funciones.controlar_vida();
-            barra_interna[1].style.setProperty('width',parseInt(vida_propia) * 10 + 'px');
-            change_numbers_2.innerHTML = parseInt(vida_propia);
-            window.setTimeout(mostrar_options, 3000);
+            functions.control_life();
+            inside_bar[1].style.setProperty('width',parseInt(own_life) * 10 + 'px');
+            change_numbers_2.innerHTML = parseInt(own_life);
+            window.setTimeout(show_options, 3000);
             break;
           case 2:
             grid_options[0].style.setProperty('display','grid');
@@ -202,18 +202,18 @@ let funciones = {
             text.innerHTML = 'A luchar!!!';
             break;    
         }
-        if(vida_enemigo == 0 || vida_propia == 0){
+        if(enemy_life == 0 || own_life == 0){
             grid_options[0].style.setProperty('display','none');
             text.style.setProperty('width','94%');
-            funciones.controlar_vida(); 
+            functions.control_life(); 
         }
-        estado_del_turno++;
+        turn_moment++;
       }
     }
   },
 
 
-  cambiar_song(){
+  change_song(){
     sound_state++;
     if(sound_state > 3){
       sound_state = 0;
@@ -255,7 +255,7 @@ let name = document.getElementsByClassName('name'),
     action = document.getElementsByClassName('action'),
     grid_options = document.getElementsByClassName('grid_options'),
     start_section = document.getElementsByClassName('start_section'),
-    barra_interna = document.getElementsByClassName('barra_interna'),
+    inside_bar = document.getElementsByClassName('inside_bar'),
     button_ending = document.getElementsByClassName('button_ending'),
     icon_transition = document.getElementsByClassName('icon_transition');
 
@@ -265,8 +265,8 @@ let final_space = document.querySelector('.final_space'),
                     
 let sound_state = 0,
     sound_viewer = 0,    
-    vida_enemigo = parseInt(document.getElementById('vida_max_1').innerHTML),
-    vida_propia = parseInt(document.getElementById('vida_max_2').innerHTML),
+    enemy_life = parseInt(document.getElementById('vida_max_1').innerHTML),
+    own_life = parseInt(document.getElementById('vida_max_2').innerHTML),
     ataque_random = [' "háblale a la mano"', 'a crisis existencial', ' televisor', 'a reseña',  
                      'a peluca', ' comentario hiriente', ' Afedo cámate pofavo', 
                      'a indirecta', ' rosario', ' ecoladrillo', 'a solicitud de amistad', 
@@ -274,13 +274,13 @@ let sound_state = 0,
  
                      
 // Eventos
-action[0].addEventListener('click',funciones.resta_al_enemigo_1);
-action[1].addEventListener('click',funciones.resta_al_enemigo_2);
-action[2].addEventListener('click',funciones.resta_al_enemigo_3);
-action[3].addEventListener('click',funciones.resta_al_enemigo_4);
-content_sound.addEventListener('click',funciones.control_sound);
-music_4_battle.addEventListener('ended',funciones.cambiar_song);
-start_button.addEventListener('click',funciones.start_game);
+action[0].addEventListener('click',functions.less_enemy_life_1);
+action[1].addEventListener('click',functions.less_enemy_life_2);
+action[2].addEventListener('click',functions.less_enemy_life_3);
+action[3].addEventListener('click',functions.less_enemy_life_4);
+content_sound.addEventListener('click',functions.control_sound);
+music_4_battle.addEventListener('ended',functions.change_song);
+start_button.addEventListener('click',functions.start_game);
 
 function newFunction() {
   button_ending[0].style.setProperty('display', 'none');
